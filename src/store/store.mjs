b17bb2edb0adf29ecf6789998947a02d441bb949
quan <quan_xiaoxiao@qq.com>
2024-04-19 09:39:20 +0800

    xxx

--- src/store/store.mjs
+++ src/store/store.mjs
@@ -13,6 +13,14 @@ const store = createStore({
       type: 'string',
       minLength: 1,
     },
+    'block.dir': {
+      type: 'string',
+      minLength: 1,
+    },
+    'block.tempDir': {
+      type: 'string',
+      minLength: 1,
+    },
     'cipher.algorithm': {
       enum: ['aes-256-ctr'],
     },
