2962b971a0f5fd7f01092698998df928b64ff189
quan <quan_xiaoxiao@qq.com>
2025-06-18 09:29:48 +0800

    xxx

--- src/controllers/resource/updateResource.mjs
+++ src/controllers/resource/updateResource.mjs
@@ -1,36 +1,32 @@
-import { isValidObjectId } from '@quanxiaoxiao/mongo';
 import createError from 'http-errors';
 
-import getResourceById from '../../controllers/resource/getResourceById.mjs';
 import logger from '../../logger.mjs';
 import {
   Entry as EntryModel,
   Resource as ResourceModel,
 } from '../../models/index.mjs';
+import getResourceById from './getResourceById.mjs';
 
 export default async (resourceItem, input) => {
-  if (Object.hasOwnProperty.call(input, 'name')
-    && (!input.name || input.name.trim() === '')
-  ) {
-    throw createError(400, 'resource name is empty');
+  const resourceItemNext = Object.assign({}, resourceItem.toObject?.() || resourceItem, input);
+  const tempInstance = new ResourceModel(resourceItemNext);
+  try {
+    await tempInstance.validate();
+  } catch (error) {
+    throw createError(400, JSON.stringify(error.errors));
   }
-  if (Object.hasOwnProperty.call(input, 'entry')) {
-    if (!isValidObjectId(input.entry)) {
+  if (resourceItemNext.entry.toString() !== resourceItem.entry.toString()) {
+    const entryItem = await EntryModel.findOne({
+      _id: input.entry,
+      invalid: {
+        $ne: true,
+      },
+    });
+    if (!entryItem) {
       throw createError(403, `\`${input.entry}\` entry not exist`);
     }
-    if (input.entry !== resourceItem.entry.toString()) {
-      const entryItem = await EntryModel.findOne({
-        _id: input.entry,
-        invalid: {
-          $ne: true,
-        },
-      });
-      if (!entryItem) {
-        throw createError(403, `\`${input.entry}\` entry not exist`);
-      }
-    }
   }
-  await ResourceModel.updateOne(
+  const modifiedCount = await ResourceModel.updateOne(
     {
       _id: resourceItem._id,
       invalid: {
@@ -43,6 +39,9 @@ export default async (resourceItem, input) => {
       },
     },
   );
+  if (modifiedCount === 0) {
+    throw createError(404);
+  }
   logger.warn(`\`${resourceItem._id.toString()}\` updateResource \`${JSON.stringify(input)}\``);
   return getResourceById(resourceItem._id);
 };
